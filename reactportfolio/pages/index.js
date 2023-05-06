7432d69957cdbbb7ca20fd8906f6de0bf3d0c92a
outman1998 <outmanmounir98@gmail.com>
2023-05-06 14:27:10 +0200

    project setup

---
+++ reactportfolio/pages/index.js
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+
+    <>
+
+      <main>
+        <section className=' bg-blue-700'>
+          <p>hej</p>
+        </section>
+      </main>
+
+    </>
+
+  )
+}
