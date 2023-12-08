26fa4480f6318a33eaa738ac58c82abbb3b1c371
outman1998 <outmanmounir98@gmail.com>
2023-12-08 19:08:19 +0100

    added technologies to my about section

--- pages/components/Intro.js
+++ pages/components/Intro.js
@@ -4,7 +4,6 @@ import Image from 'next/image'
 import foto from '../../public/self.jpg'
 import { FiLinkedin } from "react-icons/fi";
 import { FiGithub } from "react-icons/fi";
-import Link from 'next/link';
 
 export default function Intro() {
 
@@ -28,13 +27,14 @@ export default function Intro() {
 
             <div className='lg:pr-10 lg:w-1/2 pt-10 lg:pt-0'>
                 <div className='text-center lg:text-start'>
-                <h2 className=' text-6xl lg:py-2 text-[#2d2e32] dark:text-white font-bold '>Front-End React Developer</h2>
+                <h2 className=' text-7xl lg:py-2 text-[#2d2e32] dark:text-white font-extrabold	 '>Front-End Developer</h2>
 
                 <p className='text-md pt-5 pb-3 lg:py-5 lg:px-0 leading-8 dark:text-white md:text-xl mx-auto text-[#555555]'>
                 Hi, I'm Outman. Currently studying web development with focus on webapps, single page applications, database management & fullstack development.
                 </p>
                 </div>
                 <div className='text-5xl flex lg:justify-start justify-center gap-6 pb-10 lg:pb-0 text-gray-600'>
+
                 <FaFacebookSquare className='cursor-pointer w-7 text-[#2d2e32] dark:text-white' onClick={() => sendToFb()}/>
                 <FiLinkedin className='cursor-pointer w-8 text-[#2d2e32] dark:text-white' onClick={() => sendTolinkedIn()} />
                 <FiGithub className='cursor-pointer w-8 text-[#2d2e32] dark:text-white' onClick={() => sendtoGithub()} />
@@ -44,27 +44,6 @@ export default function Intro() {
             <Image className='specialDiv billed m-auto w-60 lg:w-96' src={foto}/>
             </div>
 
-        </div>
-
-        <div className='skills lg:flex items-center '>
-            <p className='text-center lg:text-left lg:mr-12 lg:pr-12 lg:border-r-2 dark:border-white border-teal-600 dark:text-white text-[#2d2e32] border-b-2 pb-5 lg:mx-0 lg:border-b-0 lg:pb-0 w-fit m-auto'>Tech Stack</p>
-
-            <div className='logos '>
-                <ul className='flex gap-5 flex-wrap list-none justify-center lg:justify-start mt-5 lg:mt-0'>
-                    <li>
-                        <img src='https://skillicons.dev/icons?i=html,css' />
-                    </li>
-                    <li>
-                        <img src='https://skillicons.dev/icons?i=js,ts' />
-                    </li>
-                    <li>
-                        <img src='https://skillicons.dev/icons?i=react,next' />
-                    </li>
-                    <li>
-                        <img src='https://skillicons.dev/icons?i=tailwind,scss' />
-                    </li>
-                </ul>
-            </div>
         </div>
 
         </div>
--- pages/components/Tech.js
+++ pages/components/Tech.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaReact } from "react-icons/fa";
 
 export default function Tech() {
   return (
@@ -28,9 +29,88 @@ export default function Tech() {
             </p>
           </div>
         </div>
+
+        <div className='text-center mt-20'>
+
+          <p className='font-bold text-2xl mb-6 text-[#2d2e32] dark:text-white'>Front-end technologies I prefer using</p>
+
+          <div className='flex justify-center items-center'>
+            <div className='text-center mr-8 lg:mr-20'>
+              <img className='w-16 rounded' src={'https://www.svgrepo.com/show/353925/javascript.svg'} />
+              <p className='text-black mt-2'>javascript</p>
+            </div>
+
+            <div className='text-center mr-8 lg:mr-20'>
+              <img className='w-16 m-auto rounded-md' src={'https://www.svgrepo.com/show/354259/react.svg'} />
+              <p className='text-black mt-2'>React</p>
+            </div>
+
+            <div className='text-center mr-8 lg:mr-20'>
+              <img className='w-16 m-auto' src={'https://www.svgrepo.com/show/452228/html-5.svg'} />
+              <p className='text-black mt-2'>HTML5</p>
+            </div>
+
+            <div className='text-center'>
+              <img className='w-16 m-auto' src={'https://www.svgrepo.com/show/452185/css-3.svg'} />
+              <p className='text-black mt-2'>CSS</p>
+            </div>
+          </div>
+
+          <div className='flex mt-5 justify-center items-center'>
+            <div className='text-center mr-8 lg:mr-20'>
+              <img className='w-16 rounded' src={'https://www.svgrepo.com/show/378440/nextjs-fill.svg'} />
+              <p className='text-black mt-2'>Next.js</p>
+            </div>
+
+            <div className='text-center mr-8 lg:mr-20'>
+              <img className='w-16 m-auto rounded-md' src={'https://www.svgrepo.com/show/452210/git.svg'} />
+              <p className='text-black mt-2'>Git</p>
+            </div>
+
+            <div className='text-center mr-8 lg:mr-20'>
+              <img className='w-16 m-auto' src={'https://www.svgrepo.com/show/374068/scss.svg'} />
+              <p className='text-black mt-2'>SCSS</p>
+            </div>
+
+            <div className='text-center'>
+              <img className='w-16 m-auto' src={'https://www.svgrepo.com/show/374118/tailwind.svg'} />
+              <p className='text-black mt-2'>Tailwind</p>
+            </div>
+          </div>
+
+        </div>
+
+        <div className='text-center mt-16'>
+
+          <p className='font-bold text-2xl mb-6 text-[#2d2e32] dark:text-white'>Other technologies</p>
+
+          <div className='flex justify-center items-center'>
+
+            <div className='text-center mr-8 lg:mr-20'>
+              <img className='w-16 rounded' src={'https://www.svgrepo.com/show/452088/php.svg'} />
+              <p className='text-black mt-2'>PHP</p>
+            </div>
+
+            <div className='text-center mr-8 lg:mr-20'>
+              <img className='w-16 m-auto rounded-md' src={'https://www.svgrepo.com/show/353985/laravel.svg'} />
+              <p className='text-black mt-2'>Laravel</p>
+            </div>
+
+            <div className='text-center mr-8 lg:mr-20'>
+              <img className='w-16 m-auto' src={'https://www.svgrepo.com/show/373848/mysql.svg'} />
+              <p className='text-black mt-2'>MySQL</p>
+            </div>
+
+            <div className='text-center'>
+              <img className='w-16 m-auto' src={'https://www.svgrepo.com/show/303266/nodejs-icon-logo.svg'} />
+              <p className='text-black mt-2'>NodeJS</p>
+            </div>
+
+          </div>
+
+        </div>
+
       </div>
-
-
 
     </section>
   )
